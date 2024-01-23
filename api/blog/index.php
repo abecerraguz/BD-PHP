65dc145881c9b19874b56d3ab09fc549f04c823a
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2024-01-23 15:00:56 -0300

    Se guardan los cambios platilla de inicio PHP

---
+++ api/blog/index.php
@@ -0,0 +1,26 @@
+
+<?php
+
+/*
+
+    API son las siglas de "Interfaz de Programación de Aplicaciones" en inglés, y se refiere a un conjunto de reglas y herramientas que permiten la interacción entre diferentes software. Una API define los métodos y estructuras de datos que los desarrolladores pueden utilizar para interactuar con un servicio o aplicación específica, sin necesidad de entender la implementación interna detallada de esa aplicación.
+
+    En otras palabras, una API proporciona una forma estandarizada y estructurada para que diferentes aplicaciones se comuniquen entre sí. Esto facilita la integración de servicios y la construcción de aplicaciones más complejas al permitir que diferentes componentes interactúen de manera eficiente.
+
+    Las API pueden adoptar diversas formas, como:
+
+    API web: Se utilizan comúnmente en el desarrollo web para permitir que aplicaciones cliente (como aplicaciones web o móviles) se comuniquen con servicios en línea a través de solicitudes HTTP.
+
+    API de bibliotecas: Proporcionan funciones y procedimientos específicos que los desarrolladores pueden utilizar al construir aplicaciones utilizando un lenguaje de programación en particular.
+
+    API de sistema operativo: Permiten que las aplicaciones interactúen con las funciones del sistema operativo, como la gestión de archivos o el acceso a recursos del sistema.
+
+    API de bases de datos: Facilitan la comunicación entre aplicaciones y bases de datos, permitiendo operaciones como la lectura, escritura y actualización de datos en una base de datos.
+
+    En resumen, una API proporciona un conjunto de reglas y herramientas para que aplicaciones y servicios se comuniquen entre sí de manera efectiva, facilitando la integración y la construcción de sistemas más complejos.
+
+*/
+
+    
+
+    ?>
